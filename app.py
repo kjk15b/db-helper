5e0e18dafec39eccf2f1f95781aa5656bc257b66
Kolby Kiesling <kjk15b@acu.edu>
2022-02-20 09:40:57 -0600

    debugging

--- app.py
+++ app.py
@@ -24,5 +24,5 @@ def ingestRoute(sensor):
 
 
 if __name__ == '__main__':
-    app.run(debug=sys.argv[1], host=sys.argv[2],
-    port=sys.argv[3])
+    app.run(debug=True, host="0.0.0.0",
+    port=5050)
