f6524480d864d017d7cdd8ae344c15accd4c0334
Kolby Kiesling <kjk15b@acu.edu>
2022-02-20 09:29:35 -0600

    add simple frontend to ingest data

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+import sys
+
+app = Flask(__name__)
+dataStream = {"UR" : list(),
+              "UL" : list(),
+              "LR" : list(),
+              "LL" : list()}
+upperLimit = sys.argv[4]
+
+
+app.route('/data/ingest/<sensor>', methods=["POST"])
+def ingestRoute(sensor):
+    if request.method == "POST":
+        data = request.form.to_dict()
+        value = data['data']
+        print("Received: {} {} Hz".format(sensor, value))
+        if len(dataStream[sensor]) > upperLimit:
+            dataStream[sensor].pop(0)
+        dataStream[sensor].append(value)
+        return "Received - Got it"
+    return "Nothing to see here"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=sys.argv[1], host=sys.arv[2],
+    port=sys.argv[3])
---
+++ boot.sh
@@ -0,0 +1,8 @@
+####################
+# 1 - Debug mode
+# 2 - Host
+# 3 - Port
+# 4 - Upper Limit
+####################
+
+python app.py $1 $2 $3 $4
